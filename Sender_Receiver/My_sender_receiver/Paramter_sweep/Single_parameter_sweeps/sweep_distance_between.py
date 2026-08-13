efdbc3162606a29a5debafdae345c886dc6389e6
swetanssinha-cloud <swetan.s.sinha@gmail.com>
2026-08-13 19:19:14 -0400

    Distance Swept Improved
    After seeing that the three values I ran work, I want to run a full sweep with the improvements:
    
    Those include the following:
    OPENBLAS_NUM_THREADS=1, OMP_NUM_THREADS=1
    
    and a couple of other things to make sure things do not compete for threads

--- Sender_Receiver/My_sender_receiver/Paramter_sweep/Single_parameter_sweeps/sweep_distance_between.py
+++ Sender_Receiver/My_sender_receiver/Paramter_sweep/Single_parameter_sweeps/sweep_distance_between.py
@@ -11,8 +11,8 @@ from sweep_core import SweepConfig, run
 
 cfg = SweepConfig(
     sweep_parameter="distance_between",
-    sweep_values=np.array([200.0, 300.0, 400.0,]),
-)# 500.0, 600.0, 700.0, 800.0, 900.0, 1000.0 removing these for now because of intial testing
+    sweep_values=np.array([200.0, 300.0, 400.0,500.0, 600.0, 700.0, 800.0, 900.0, 1000.0]),
+)#  removing these for now because of intial testing
 
 if __name__ == "__main__":
     run(cfg)
