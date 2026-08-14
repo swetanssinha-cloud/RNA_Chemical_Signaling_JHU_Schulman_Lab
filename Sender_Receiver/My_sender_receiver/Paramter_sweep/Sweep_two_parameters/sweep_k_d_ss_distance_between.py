d5f0825bc8fc5d8ea334e328500fbb2d43cf9d77
swetanssinha-cloud <swetan.s.sinha@gmail.com>
2026-08-14 12:06:24 -0400

    More space + Fix syntax
    Had to boost the mem up and the cores down + fix a careless copy and paste error on my end

--- Sender_Receiver/My_sender_receiver/Paramter_sweep/Sweep_two_parameters/sweep_k_d_ss_distance_between.py
+++ Sender_Receiver/My_sender_receiver/Paramter_sweep/Sweep_two_parameters/sweep_k_d_ss_distance_between.py
@@ -3,7 +3,7 @@ from Sweep_two_core import TwoParamSweepConfig, run
 
 cfg = TwoParamSweepConfig(
     parameter_one="distance_between",
-    values_one=np.linspace(100,1000,10) * 3e-4,
+    values_one=np.linspace(100, 1000, 10),
     parameter_two="k_d_ss",
     values_two=np.linspace(0, 1, 10) * 3e-4
 )
